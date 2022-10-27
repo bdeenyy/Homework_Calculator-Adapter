98b57ac81643aa7e6d022dc3ca6afec09fe9d388
bdeenyy <109365710+bdeenyy@users.noreply.github.com>
2022-10-27 10:50:38 +0300

    Task complete

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.sum(144, 72));
+        System.out.println(calc.mult(12, 18));
+        System.out.println(calc.pow(6, 3));
+    }
+}
